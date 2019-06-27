1ba0628b64f8ab0138c63224d3d44882e44fac00
BitlyTwiser <joshua.groeschl@tutanota.com>
2019-06-26 20:07:35 -0700

    Modified setup.py to contain thje proper function name

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'ipfinder = externip:external_IP',
+            'ipfinder = externip:ipfinder',
         ]
     },
     package_dir={'': 'src'},
