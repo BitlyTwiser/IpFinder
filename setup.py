50a710bde84ad811143bb81ee2a4ca793c077cba
BitlyTwiser <joshua.groeschl@tutanota.com>
2019-06-26 20:12:22 -0700

    Modifid setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'ipfinder = externip:ipfinder',
+            'ipfinder = ipfinder:externip',
         ]
     },
     package_dir={'': 'src'},
