cfb1730e882ac36bfb8122206976c78481a45c56
BitlyTwiser <joshua.groeschl@tutanota.com>
2019-06-26 20:24:01 -0700

    Modfied setup.py to attempt alternate file path

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'ipfinder = ipfinder:externip',
+            'ipfinder = src.ipfinder.externip:external_IP',
         ]
     },
     package_dir={'': 'src'},
