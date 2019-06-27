651daef1c709f6f6b41296c6f6543da56cc9e288
BitlyTwiser <joshua.groeschl@tutanota.com>
2019-06-26 20:20:58 -0700

    adjust path to tscript

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'ipfinder = ipfinder:externip:external_IP',
+            'ipfinder = ipfinder:externip',
         ]
     },
     package_dir={'': 'src'},
